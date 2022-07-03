2165af7058d63aea8aa7baed6c0e86c2458fa74a
ShalomShahzad <shalomshahzad33@gmail.com>
2022-07-03 09:19:49 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
-import React, { useState } from 'react';
-import Alert from './components/Alert';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import React, { useState } from 'react'
+import Alert from './Components/Alert';
+import About from './Components/About';
 
- 
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-          setAlert(null);
-      }, 1500);
+  const showAlert =(message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    }, 1000)
   }
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
+  const toggleMode = () =>{
+    if (mode === 'light') {
+      setMode('dark')
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
+      showAlert("Dark mode has been enable", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
+      showAlert("Light mode has been enable", "success");
     }
+
   }
+
   return (
     <>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
-    <Alert alert={alert}/>
-    <div className="container my-3">
-    {/* /users --> Component 1
-        /users/home --> Component 2 */}
-            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
-            <About mode={mode} />
-    </div>
-    </> 
+  <Navbar Title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+  <Alert alert={alert}/>
+  <div className="container my-3">
+  <TextForm heading="Enter The Text To Analyze Below" mode={mode} showAlert={showAlert}/>
+  <About mode={mode}/>
+  </div>
+    </>
   );
+ 
 }
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,29 @@
 import React from 'react'
 
 export default function About(props) {
-    let myStyle = {
-        color: props.mode ==='dark'?'white':'#042743',
-        backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
-    }
-    
-    return (
-        <div className="container">
-            <div className="accordion" id="accordionExample">
-                <div className="accordion-item">
-                    <h2 className="accordion-header" id="headingOne">
-                    <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    <strong>About Text-Utils</strong>
-                    </button>
-                    </h2>
-                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                    <div className="accordion-body" style={myStyle}>
-                    <strong>Text-Utils is a text utility static site made using React. It allow to modify your text in various ways, Whether it is making in uppercase, lowercase, copy text, clear text or remove extra spaces.
-                        Made By - Shalom Shahzad
-                    </strong>
+  let myStyle = {
+     color: props.mode === 'dark'?'white':'#042743',
+     backgroundColor: props.mode === 'dark'?'#042743':'white'
+  }
 
-                    </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    )
+  return (
+    <div className='container' style={myStyle}>
+        <div className="accordion" id="accordionExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingOne">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        About TextUtils
+      </button>
+    </h2>
+    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div className="accordion-body" style={myStyle}>
+        <strong>This is a TextUtils App. It is created by using React. In this app you input some text and this app will tell you amount of words in it, amount of characters, and will show you the preview. You can use "Convert to Uppercase", "Convert to Lowercase", and "Clear Text" buttons to convert the text into Uppercase, Lowercase, or clear the text respectively.
+          Developed by Shalom Shahzad
+        </strong>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
 }
