a45ca44135592c8a7ed0364df6ac4a903c34c209
ShalomShahzad <shalomshahzad33@gmail.com>
2022-07-03 10:06:33 +0500

    Updates

---
+++ precache-manifest.4a7209de50a7b025657efeb5f95b41ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "282c5933ff46af7acfe5",
+    "url": "/TextUtils-Final/static/css/main.72b2aa52.chunk.css"
+  },
+  {
+    "revision": "282c5933ff46af7acfe5",
+    "url": "/TextUtils-Final/static/js/main.282c5933.chunk.js"
+  },
+  {
+    "revision": "833a7611ce42bb2c9054",
+    "url": "/TextUtils-Final/static/js/1.833a7611.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils-Final/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "5290b92de36f066cbd3f",
+    "url": "/TextUtils-Final/static/js/runtime~main.5290b92d.js"
+  },
+  {
+    "revision": "9150e4aa9c407274b73fb72108ba5f56",
+    "url": "/TextUtils-Final/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-react-app/precache-manifest.e5452dcca88592a7f8ee10d8e89c5ba5.js"
+  "/TextUtils-Final/precache-manifest.4a7209de50a7b025657efeb5f95b41ef.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-react-app/index.html", {
+workbox.routing.registerNavigationRoute("/TextUtils-Final/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
